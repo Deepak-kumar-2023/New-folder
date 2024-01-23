fd2fb228e667aca043b0e8fb9e7855be3b98d5b9
Deepak <kumardeeepak392@gmail.com>
2024-01-23 18:56:35 +0530

    functions_and_js

---
+++ functionss.js
@@ -0,0 +1,39 @@
+//singleton
+
+//Objects
+
+// Object.create;
+// objects litreal
+// performing symbol
+
+
+
+// const gyan=Symbol("key1")
+// const jsUser={
+//     user:"deepak",
+//     [gyan]:"keyTwo",
+//     "user_full_name":"deepak_kumar",
+//     roll_no:25 ,
+//     email:"deepak@google.com",
+//     branch:"cse"
+// }
+// console.log(jsUser);
+// console.log(jsUser.email);
+// console.log(jsUser.user);
+// console.log(jsUser.roll_no);
+
+// console.log(jsUser["roll_no"])
+// console.log(jsUser["branch"])
+// console.log(jsUser["email"])
+// console.log(jsUser["user_full_name"])
+// jsUser.email="deepak@microsoft.com",
+// console.log(jsUser.email)
+// Object.freeze(jsUser.email)
+// jsUser.email="deepak@amaijon.com"
+// console.log(jsUser)
+// console.log(jsUser.email)
+// console.log(jsUser[gyan])
+// jsUser.grateeting=function(){
+// console.log(`email of vivek is ${jsUser.email}`);
+// }
+// console.log(jsUser.grateeting());
